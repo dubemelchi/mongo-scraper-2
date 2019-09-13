d8a8f437b63188596000d26760e777b4e704194d
dubemelchi <dubemelchi@gmail.com>
2019-09-13 14:18:30 -0400

    setup and test server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// in this file we will setup our node server and include our routes - Mel
+// in this file i will setup our node server and include our routes - Mel
+
+// require the dependencies
+const express = require("express");
+
+// setting port to either host's designated port, or 3000!
+const PORT = process.env.PORT || 3000;
+
+// initialize our express app
+const app = express();
+
+// setup our express router
+var router = express.Router();
+
+// set public folder as static
+app.use(express.static(__dirname + "/public"));
+
+// pass requests through middleware
+app.use(router);
+
+// listen on this port SAMUEL JACKSON!!!
+app.listen(PORT, function() {
+  console.log("Sam Jackson is listening on port: " +  PORT);
+});
